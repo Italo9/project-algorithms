083ef941707dd5d1567c7505e675220e55259513
Italo <contatoitallo@hotmail.com>
2022-11-28 11:12:13 -0300

    feat: :sparkles: Implementa função que determina se uma palavra é um palíndromo ou não por meio de solução iterativa

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,9 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not isinstance(word, str) or len(word) == 0:
+        return False
+    # invertendo sequência usando notação: (referencia bibliografica abaixo)
+    #  https://forum.freecodecamp.org/t/how-to-use-python-slice-with-the-start-stop-and-step-arguments-explained-with-examples/19202
+    if word == word[::-1]:
+        return True
+    if word != word[::-1]:
+        return False
