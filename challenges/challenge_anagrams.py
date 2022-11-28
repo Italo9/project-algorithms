3a55a8fc012abdc889d5ac2270c653deaec494b3
Italo <contatoitallo@hotmail.com>
2022-11-28 12:07:05 -0300

    fix: :bug: Muda estrutura do if e posição da função

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,31 @@
+#  referencia bibliografica:
+# https://joaoarthurbm.github.io/eda/posts/quick-sort/
+# https://stackoverflow.com/questions/18262306/quicksort-with-python
+
+
+def quicksort(lista, left, right):
+    if len(lista) == 1:
+        return lista
+
+    if left < right:
+        index = partition(lista, left, right)
+        quicksort(lista, left, index - 1)
+        quicksort(lista, index + 1, right)
+
+
+def partition(lista, left, right):
+    pivot = lista[right]
+    i = left - 1
+
+    for j in range(left, right):
+        if lista[j] <= pivot:
+            i = i + 1
+            lista[i], lista[j] = lista[j], lista[i]
+    lista[i + 1], lista[right] = lista[right], lista[i + 1]
+
+    return i + 1
+
+
 def is_anagram(first_string, second_string):
     primeira = first_string.lower()
     segunda = second_string.lower()
@@ -19,30 +47,3 @@ def is_anagram(first_string, second_string):
     if lista_primeira == lista_segunda:
         return (join_lista_primeira, join_lista_segunda, True)
     return (join_lista_primeira, join_lista_segunda, False)
-
-#  referencia bibliografica:
-# https://joaoarthurbm.github.io/eda/posts/quick-sort/
-# https://stackoverflow.com/questions/18262306/quicksort-with-python
-
-
-def quicksort(lista, left, right):
-    if len(lista) == 1:
-        return lista
-
-    if right > left:
-        index = partition(lista, left, right)
-        quicksort(lista, left, index - 1)
-        quicksort(lista, index + 1, right)
-
-
-def partition(lista, left, right):
-    pivot = lista[right]
-    i = left - 1
-
-    for j in range(left, right):
-        if lista[j] <= pivot:
-            i = i + 1
-            lista[i], lista[j] = lista[j], lista[i]
-    lista[i + 1], lista[right] = lista[right], lista[i + 1]
-
-    return i + 1
