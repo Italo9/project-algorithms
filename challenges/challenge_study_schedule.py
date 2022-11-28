75785e3eeae0bf201af8ffc794345ade1d77d4c5
Italo <contatoitallo@hotmail.com>
2022-11-28 09:42:50 -0300

    feat: :sparkles: Implementa função challenge_study_schedule() para retornar a quantidade de pessoas estudantes acessando o conteúdo da plataforma de estudos no mesmo horário se, e somente se, as tuplas dentro do array permanence_period forem formadas por dados do tipo int, bem como o type do segundo parametro target_time, que também deverá ser um inteiro.

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,15 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if (
+        not (isinstance(target_time, int))
+        or not (isinstance(permanence_period, list))
+        or (len(permanence_period) == 0)
+    ):
+        return None
+    acc = 0
+
+    for deltaS0, deltaS in permanence_period:
+        if not (isinstance(deltaS0, int) and isinstance(deltaS, int)):
+            return None
+        elif deltaS0 <= target_time <= deltaS:
+            acc += 1
+    return acc
