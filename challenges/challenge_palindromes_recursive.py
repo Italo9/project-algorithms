600ccf1cd43b50688c294815828053d6146628c1
Italo <contatoitallo@hotmail.com>
2022-11-28 10:47:21 -0300

    feat: :sparkles: Implementa função que determina se uma palavra é um palíndromo ou não.

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,10 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not isinstance(word, str) or len(word) == 0:
+        return False
+    if word[low_index] != word[high_index]:
+        return False
+    if high_index <= low_index:
+        return True
+    if high_index + 1 > low_index:
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+    return True
